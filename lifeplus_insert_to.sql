2eb7d3cc0420205b11babedaa57c31dcb0b62205
marianegasparotto <marigasparotto141@hotmail.com>
2023-08-21 13:53:07 -0300

    correção id da tabela medicamentos_injetaveis

--- lifeplus_insert_to.sql
+++ lifeplus_insert_to.sql
@@ -20,13 +20,13 @@ insert into medicamentos(id, nome, concentracao, apresentacao, qtd_por_unidade, 
 select * from medicamentos_injetaveis;
 
 insert into medicamentos_injetaveis(id, nome, concentracao, apresentacao, qtd_por_unidade, marca, descricao, preco, preco_com_desconto, tipo_de_aplicacao) values
-(1, 'Dipirona Sódica', '500mg/ml', 'Ampola', 10, 'MedPharma', 'Analgésico e antitérmico injetável para alívio de dores e febre.', 19.99, 15.99, 'Intramuscular'),
-(2, 'Metamizol Sódico', '1g/2ml', 'Ampola', 5, 'Farmalife', 'Analgésico e antipirético utilizado para controle de dores intensas e febre.', 25.50, 20.99, 'Endovenosa'),
-(3, 'Dexametasona', '4mg/ml', 'Ampola', 20, 'PharmaPlus', 'Corticosteroide utilizado para reduzir inflamações e tratar diversas condições médicas.', 18.75, 14.99, 'Intradérmica'),
-(4, 'Ampicilina Sódica', '500mg', 'Ampola', 25, 'Medicor', 'Antibiótico de amplo espectro para tratamento de infecções bacterianas.', 12.99, 9.99, 'Subcutânea'),
-(5, 'Sulfato de Magnésio', '10% - 2ml', 'Ampola', 10, 'PharmaCure', 'Suplemento de magnésio utilizado em casos de deficiência ou tratamento de condições específicas.', 27.50, 22.99, 'Intramuscular'),
-(6, 'Cloridrato de Tramadol', '50mg/ml', 'Ampola', 10, 'Genérico', 'Analgésico opioide utilizado para o tratamento de dores moderadas a graves.', 21.30, 16.99, 'Endovenosa'),
-(7, 'Haloperidol', '5mg/ml', 'Ampola', 5, 'BioMed', 'Antipsicótico utilizado para o tratamento de transtornos mentais como esquizofrenia e agitação.', 14.99, 11.99, 'Intramuscular'),
-(8, 'Cloridrato de Prometazina', '50mg/ml', 'Ampola', 10, 'MedPharma', 'Antialérgico e antiemético utilizado no controle de alergias e náuseas.', 9.99, 7.99, 'Endovenosa'),
-(9, 'Fosfato de Sódio Monobásico', '15mmol/5ml', 'Ampola', 20, 'Farmalife', 'Laxante utilizado para o esvaziamento intestinal antes de procedimentos médicos.', 16.50, 12.99, 'Intradérmica'),
-(10, 'Succinato de Metilprednisolona', '40mg/ml', 'Ampola', 10, 'PharmaPlus', 'Corticosteroide utilizado para reduzir inflamações e tratar diversas condições médicas.', 23.99, 18.99, 'Subcutânea');
+(11, 'Dipirona Sódica', '500mg/ml', 'Ampola', 10, 'MedPharma', 'Analgésico e antitérmico injetável para alívio de dores e febre.', 19.99, 15.99, 'Intramuscular'),
+(12, 'Metamizol Sódico', '1g/2ml', 'Ampola', 5, 'Farmalife', 'Analgésico e antipirético utilizado para controle de dores intensas e febre.', 25.50, 20.99, 'Endovenosa'),
+(13, 'Dexametasona', '4mg/ml', 'Ampola', 20, 'PharmaPlus', 'Corticosteroide utilizado para reduzir inflamações e tratar diversas condições médicas.', 18.75, 14.99, 'Intradérmica'),
+(14, 'Ampicilina Sódica', '500mg', 'Ampola', 25, 'Medicor', 'Antibiótico de amplo espectro para tratamento de infecções bacterianas.', 12.99, 9.99, 'Subcutânea'),
+(15, 'Sulfato de Magnésio', '10% - 2ml', 'Ampola', 10, 'PharmaCure', 'Suplemento de magnésio utilizado em casos de deficiência ou tratamento de condições específicas.', 27.50, 22.99, 'Intramuscular'),
+(16, 'Cloridrato de Tramadol', '50mg/ml', 'Ampola', 10, 'Genérico', 'Analgésico opioide utilizado para o tratamento de dores moderadas a graves.', 21.30, 16.99, 'Endovenosa'),
+(17, 'Haloperidol', '5mg/ml', 'Ampola', 5, 'BioMed', 'Antipsicótico utilizado para o tratamento de transtornos mentais como esquizofrenia e agitação.', 14.99, 11.99, 'Intramuscular'),
+(18, 'Cloridrato de Prometazina', '50mg/ml', 'Ampola', 10, 'MedPharma', 'Antialérgico e antiemético utilizado no controle de alergias e náuseas.', 9.99, 7.99, 'Endovenosa'),
+(19, 'Fosfato de Sódio Monobásico', '15mmol/5ml', 'Ampola', 20, 'Farmalife', 'Laxante utilizado para o esvaziamento intestinal antes de procedimentos médicos.', 16.50, 12.99, 'Intradérmica'),
+(20, 'Succinato de Metilprednisolona', '40mg/ml', 'Ampola', 10, 'PharmaPlus', 'Corticosteroide utilizado para reduzir inflamações e tratar diversas condições médicas.', 23.99, 18.99, 'Subcutânea');
